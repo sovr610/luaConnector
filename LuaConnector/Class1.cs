79b407bed0efedc31bc29a6ad9e0faad6bec35f5
Parker Bidigare <work.simpleintelligence@gmail.com>
2019-03-29 18:57:24 -0400

    init

---
+++ LuaConnector/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLua;
+
+
+namespace LuaConnector
+{
+    public class luaConnector
+    {
+        private Lua lua;
+
+        public luaConnector()
+        {
+            lua = new Lua();
+        }
+
+        public void runLineOfCode(string code)
+        {
+            try
+            {
+                lua.DoString(code);
+            }
+            catch (Exception i)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+
+        public bool addFunc(string func, object obj)
+        {
+
+            try
+            {
+                lua.RegisterFunction(func, obj, obj.GetType().GetMethod(func));
+                return true;
+            }
+            catch (Exception i)
+            {
+                Console.Write(i.Message);
+                Console.WriteLine("_error Script: func: " + func + "_class: " + obj);
+                return false;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ MyScriptingLanguage/Program.cs
@@ -0,0 +1,34 @@
+﻿using LuaConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using LuaConnector;
+
+namespace MyScriptingLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prm = new Program();
+            luaConnector lua = new luaConnector();
+            lua.addFunc("sayHello", prm);
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if(command == "/shutdown")
+                {
+                    break;
+                }
+                lua.runLineOfCode(command);
+            }
+        }
+        
+        public void sayHello()
+        {
+            Console.WriteLine("say Hello");
+        }
+    }
+}
